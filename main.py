dc31bf0c55544c5224fe7d38a4d1076eb47eb38a
ferdejnavarro <ferdejnavarro@gmail.com>
2024-03-01 14:36:30 -0600

    Day 24: Pages added for each topic

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ for index,row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
     pdf.line(10,21, 200,21)
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
 
 pdf.output('output.pdf')
