a4bc77444bdf7dbcc28fa6de719dcf26433b3377
ferdejnavarro <ferdejnavarro@gmail.com>
2024-03-26 22:30:59 -0600

    Add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# PDF Notebook
+
+This project is able to create a notebook from a csv file. The csv indicates 
+the topics and the pages each one is having, feel free to modify it in your 
+favor.
--- main.py
+++ main.py
@@ -7,25 +7,24 @@ pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv('topics.csv')
 
 
-for index,row in df.iterrows():
+for index, row in df.iterrows():
     pdf.add_page()
 
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10,21, 200,21)
-
+    pdf.line(10, 21,  200, 21)
 
     # Set the footer
     pdf.ln(260)
-    pdf.set_font(family= 'Times', style='I', size=8)
+    pdf.set_font(family='Times', style='I', size=8)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
     for i in range(row['Pages'] - 1):
         pdf.add_page()
         pdf.ln(272)
-        pdf.set_font(family= 'Times', style='I', size=8)
+        pdf.set_font(family='Times', style='I', size=8)
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
